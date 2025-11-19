01ff216aea8f2e4534ec779b6741c6ef0e268ecd
nuzwa269 <blsoha424@gmail.com>
2025-11-19 17:54:34 +0500

    Add NVB_Visa_Programs class for visa management

---
+++ admin/class-visa-programs.php
@@ -0,0 +1,76 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+class NVB_Visa_Programs {
+	public static function init() {
+		add_action( 'admin_post_nvb_save_visa', array( __CLASS__, 'save_visa' ) );
+		// Basic page
+	}
+
+	public static function visa_programs_page() {
+		// Simple wrapper page - for brevity, reuse countries template structure in future
+		include NVB_PLUGIN_DIR . 'templates/admin/visa-programs-list.php';
+	}
+
+	public static function save_visa() {
+		if ( ! current_user_can( 'manage_options' ) ) {
+			wp_die( esc_html__( 'Unauthorized', 'nvb' ) );
+		}
+		if ( empty( $_POST['nvb_visa_nonce'] ) || ! wp_verify_nonce( sanitize_text_field( wp_unslash( $_POST['nvb_visa_nonce'] ) ), 'nvb_save_visa' ) ) {
+			wp_die( esc_html__( 'Invalid nonce', 'nvb' ) );
+		}
+		global $wpdb;
+		$prefix = $wpdb->prefix;
+
+		$id = isset( $_POST['id'] ) ? intval( $_POST['id'] ) : 0;
+		$country_id = intval( $_POST['country_id'] ?? 0 );
+		$title = sanitize_text_field( $_POST['title'] ?? '' );
+		$duration = sanitize_text_field( $_POST['duration'] ?? '' );
+		$income_requirement = sanitize_text_field( $_POST['income_requirement'] ?? '' );
+		$description = wp_kses_post( $_POST['description'] ?? '' );
+		$official_link = esc_url_raw( $_POST['official_link'] ?? '' );
+
+		if ( empty( $title ) || ! $country_id ) {
+			wp_redirect( admin_url( 'admin.php?page=nvb_visa_programs&message=missing' ) );
+			exit;
+		}
+
+		if ( $id ) {
+			$wpdb->update(
+				"{$prefix}nvb_visa_programs",
+				array(
+					'country_id'         => $country_id,
+					'title'              => $title,
+					'duration'           => $duration,
+					'income_requirement' => $income_requirement,
+					'description'        => $description,
+					'official_link'      => $official_link,
+					'updated_at'         => current_time( 'mysql' ),
+				),
+				array( 'id' => $id ),
+				array( '%d', '%s', '%s', '%s', '%s', '%s', '%s' ),
+				array( '%d' )
+			);
+		} else {
+			$wpdb->insert(
+				"{$prefix}nvb_visa_programs",
+				array(
+					'country_id'         => $country_id,
+					'title'              => $title,
+					'duration'           => $duration,
+					'income_requirement' => $income_requirement,
+					'description'        => $description,
+					'official_link'      => $official_link,
+					'created_at'         => current_time( 'mysql' ),
+					'updated_at'         => current_time( 'mysql' ),
+				),
+				array( '%d', '%s', '%s', '%s', '%s', '%s', '%s' )
+			);
+		}
+
+		wp_redirect( admin_url( 'admin.php?page=nvb_visa_programs&message=success' ) );
+		exit;
+	}
+}
