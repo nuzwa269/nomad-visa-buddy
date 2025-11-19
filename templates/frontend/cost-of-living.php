8f75fe3dd843e6f6679d130d52c0aafe44cd7d29
nuzwa269 <blsoha424@gmail.com>
2025-11-19 18:17:29 +0500

    Add cost of living table template

---
+++ templates/frontend/cost-of-living.php
@@ -0,0 +1,22 @@
+```php
+<?php if ( ! defined( 'ABSPATH' ) ) exit; ?>
+<table class="nvb-col-table">
+	<thead>
+		<tr>
+			<th>Category</th>
+			<th>Monthly Cost (USD)</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr><td>Rent</td><td><?php echo esc_html( number_format( $col->rent, 2 ) ); ?></td></tr>
+		<tr><td>Food</td><td><?php echo esc_html( number_format( $col->food, 2 ) ); ?></td></tr>
+		<tr><td>Transport</td><td><?php echo esc_html( number_format( $col->transport, 2 ) ); ?></td></tr>
+		<tr><td>Internet</td><td><?php echo esc_html( number_format( $col->internet, 2 ) ); ?></td></tr>
+		<tr><td>Healthcare</td><td><?php echo esc_html( number_format( $col->healthcare, 2 ) ); ?></td></tr>
+		<tr><td>Lifestyle Score</td><td><?php echo esc_html( intval( $col->lifestyle_score ) ); ?>/100</td></tr>
+	</tbody>
+</table>
+<?php if ( ! empty( $col->notes ) ) : ?>
+	<div class="nvb-col-notes"><?php echo wp_kses_post( $col->notes ); ?></div>
+<?php endif; ?>
+```
