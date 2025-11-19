5254524c39e721d130184e7f70e5f28d51118549
nuzwa269 <blsoha424@gmail.com>
2025-11-19 18:08:57 +0500

    Add eligibility list admin template

---
+++ templates/admin/eligibility-list.php
@@ -0,0 +1,7 @@
+```php
+<?php if ( ! defined( 'ABSPATH' ) ) exit; ?>
+<div class="wrap nvb-admin-wrap">
+	<h1>Eligibility</h1>
+	<p>Manage eligibility Q&A for visa programs. Form is available via the admin actions.</p>
+</div>
+```
