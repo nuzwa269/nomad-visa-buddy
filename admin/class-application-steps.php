0614abaa18c2e9c7e74ee572ff7d487f51a51af6
nuzwa269 <blsoha424@gmail.com>
2025-11-20 18:54:01 +0500

    Update class-application-steps.php

--- admin/class-application-steps.php
+++ admin/class-application-steps.php
@@ -1,56 +1,131 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
+if ( ! defined( 'ABSPATH' ) ) exit;
 
 class NVB_Application_Steps {
+
 	public static function init() {
+
+		// Create / Update
 		add_action( 'admin_post_nvb_save_step', array( __CLASS__, 'save_step' ) );
+
+		// Soft delete
+		add_action( 'admin_post_nvb_delete_step', array( __CLASS__, 'delete_step' ) );
 	}
-	public static function steps_page() {
+
+	public static function application_steps_page() {
 		include NVB_PLUGIN_DIR . 'templates/admin/application-steps-list.php';
 	}
 
+	/**
+	 * Insert / Update Step
+	 */
 	public static function save_step() {
+
 		if ( ! current_user_can( 'manage_options' ) ) {
-			wp_die( esc_html__( 'Unauthorized', 'nvb' ) );
+			wp_die( __( 'Unauthorized', 'nvb' ) );
 		}
-		if ( empty( $_POST['nvb_step_nonce'] ) || ! wp_verify_nonce( sanitize_text_field( wp_unslash( $_POST['nvb_step_nonce'] ) ), 'nvb_save_step' ) ) {
-			wp_die( esc_html__( 'Invalid nonce', 'nvb' ) );
+
+		if ( empty( $_POST['nvb_step_nonce'] ) ||
+		     ! wp_verify_nonce( sanitize_text_field( wp_unslash( $_POST['nvb_step_nonce'] ) ), 'nvb_save_step' ) ) {
+			wp_die( __( 'Invalid nonce', 'nvb' ) );
 		}
+
 		global $wpdb;
 		$prefix = $wpdb->prefix;
 
-		$country_id = intval( $_POST['country_id'] ?? 0 );
-		$visa_program_id = intval( $_POST['visa_program_id'] ?? 0 );
+		$id          = intval( $_POST['id'] ?? 0 );
+		$country_id  = intval( $_POST['country_id'] ?? 0 );
 		$step_number = intval( $_POST['step_number'] ?? 0 );
-		$title = sanitize_text_field( $_POST['title'] ?? '' );
+		$title       = sanitize_text_field( $_POST['title'] ?? '' );
 		$description = wp_kses_post( $_POST['description'] ?? '' );
-		$external_link = esc_url_raw( $_POST['external_link'] ?? '' );
-		$screenshot_url = esc_url_raw( $_POST['screenshot_url'] ?? '' );
 
-		if ( ! $country_id || empty( $title ) ) {
+		if ( empty( $title ) || ! $country_id ) {
 			wp_redirect( admin_url( 'admin.php?page=nvb_application_steps&message=missing' ) );
 			exit;
 		}
 
-		$wpdb->insert(
+		if ( $id ) {
+
+			$wpdb->update(
+				"{$prefix}nvb_application_steps",
+				array(
+					'country_id'  => $country_id,
+					'step_number' => $step_number,
+					'title'       => $title,
+					'description' => $description,
+					'updated_at'  => current_time( 'mysql' ),
+				),
+				array( 'id' => $id ),
+				array( '%d', '%d', '%s', '%s', '%s' ),
+				array( '%d' )
+			);
+
+			$message = 'updated';
+
+		} else {
+
+			$wpdb->insert(
+				"{$prefix}nvb_application_steps",
+				array(
+					'country_id'  => $country_id,
+					'step_number' => $step_number,
+					'title'       => $title,
+					'description' => $description,
+					'is_deleted'  => 0,
+					'created_at'  => current_time( 'mysql' ),
+					'updated_at'  => current_time( 'mysql' ),
+				),
+				array( '%d', '%d', '%s', '%s', '%d', '%s', '%s' )
+			);
+
+			$message = 'created';
+		}
+
+		wp_redirect( admin_url( 'admin.php?page=nvb_application_steps&message=' . $message ) );
+		exit;
+	}
+
+	/**
+	 * Soft delete step
+	 */
+	public static function delete_step() {
+
+		if ( ! current_user_can( 'manage_options' ) ) {
+			wp_die( __( 'Unauthorized', 'nvb' ) );
+		}
+
+		if (
+			empty( $_GET['_wpnonce'] ) ||
+			! wp_verify_nonce(
+				sanitize_text_field( wp_unslash( $_GET['_wpnonce'] ) ),
+				'nvb_delete_step'
+			)
+		) {
+			wp_die( __( 'Invalid nonce', 'nvb' ) );
+		}
+
+		$id = intval( $_GET['id'] ?? 0 );
+
+		if ( ! $id ) {
+			wp_redirect( admin_url( 'admin.php?page=nvb_application_steps&message=missing' ) );
+			exit;
+		}
+
+		global $wpdb;
+		$prefix = $wpdb->prefix;
+
+		$wpdb->update(
 			"{$prefix}nvb_application_steps",
 			array(
-				'country_id'      => $country_id,
-				'visa_program_id' => $visa_program_id,
-				'step_number'     => $step_number,
-				'title'           => $title,
-				'description'     => $description,
-				'external_link'   => $external_link,
-				'screenshot_url'  => $screenshot_url,
-				'created_at'      => current_time( 'mysql' ),
-				'updated_at'      => current_time( 'mysql' ),
+				'is_deleted' => 1,
+				'updated_at' => current_time( 'mysql' ),
 			),
-			array( '%d', '%d', '%d', '%s', '%s', '%s', '%s', '%s' )
+			array( 'id' => $id ),
+			array( '%d', '%s' ),
+			array( '%d' )
 		);
 
-		wp_redirect( admin_url( 'admin.php?page=nvb_application_steps&message=success' ) );
+		wp_redirect( admin_url( 'admin.php?page=nvb_application_steps&message=deleted' ) );
 		exit;
 	}
 }
